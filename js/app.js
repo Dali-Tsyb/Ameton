8b51d4d42be809c10462b091c428021155c7fb42
Dali <lamorrre5645@gmail.com>
2024-08-31 16:10:47 +0300

    Added browser support & fixed bugs

--- js/app.js
+++ js/app.js
@@ -8,22 +8,14 @@ $(document).ready(function () {
       (isNaN(window.innerWidth) ? 0 : 0.017 * window.innerWidth);
 
    $(".button").click(function () {
-      // Сдвиг карусели влево
       $carousel.css("transform", "translateX(" + -$itemWidth + "px)");
-
-      // По завершении анимации
       setTimeout(function () {
-         // Перемещаем первый элемент в конец
          $carousel.append($(".carousel li").first());
-
-         // Убираем анимацию и сбрасываем позицию
          $carousel.css("transition", "none");
          $carousel.css("transform", "translateX(0)");
-
-         // Восстанавливаем анимацию после сброса позиции
          setTimeout(function () {
             $carousel.css("transition", "transform 0.5s ease-in-out");
          }, 50);
-      }, 500); // Время задержки должно совпадать с длительностью анимации (500 мс)
+      }, 500);
    });
 });
